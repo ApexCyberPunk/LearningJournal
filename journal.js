3a96ba05396efbc142ccf41d29a5a3981a4749db
snovacain <61154743+snovacain@users.noreply.github.com>
2023-10-20 16:48:54 -0700

    Let's Git it

--- journal.js
+++ journal.js
@@ -1,3 +1,105 @@
 
 
+//  ██████  ██████  ███    ██ ███████ ████████
+// ██      ██    ██ ████   ██ ██         ██
+// ██      ██    ██ ██ ██  ██ ███████    ██
+// ██      ██    ██ ██  ██ ██      ██    ██
+//  ██████  ██████  ██   ████ ███████    ██
+
+// ██    ██  █████  ██████  ██  █████  ██████  ██      ███████ ███████
+// ██    ██ ██   ██ ██   ██ ██ ██   ██ ██   ██ ██      ██      ██
+// ██    ██ ███████ ██████  ██ ███████ ██████  ██      █████   ███████
+//  ██  ██  ██   ██ ██   ██ ██ ██   ██ ██   ██ ██      ██           ██
+//   ████   ██   ██ ██   ██ ██ ██   ██ ██████  ███████ ███████ ███████
+
+
+
+// const pathListEl = document.querySelector('.pathList')
+// const contactListEl = document.querySelector('.contactList')
+// const projectListEl = document.querySelector('.projectList')
+// const journalListEl = document.querySelector('.journalList')
+// const dropDownItemsEl = document.querySelector('.dropdownItems')
+// const barEl = document.querySelector('.bar')
+
+// let isDisplayed = {
+//     learning: false,
+//     contact: false,
+//     projects: false,
+//     journal: false
+// }
+
+
+// ███████ ██    ██ ███████ ███    ██ ████████
+// ██      ██    ██ ██      ████   ██    ██
+// █████   ██    ██ █████   ██ ██  ██    ██
+// ██       ██  ██  ██      ██  ██ ██    ██
+// ███████   ████   ███████ ██   ████    ██
+
+
+// ██      ██ ███████ ████████ ███████ ███    ██ ███████ ██████  ███████
+// ██      ██ ██         ██    ██      ████   ██ ██      ██   ██ ██
+// ██      ██ ███████    ██    █████   ██ ██  ██ █████   ██████  ███████
+// ██      ██      ██    ██    ██      ██  ██ ██ ██      ██   ██      ██
+// ███████ ██ ███████    ██    ███████ ██   ████ ███████ ██   ██ ███████
+
+
+// document.addEventListener('click', (event)=>{
+//      event.target.dataset.learning ? showNavigation(event)
+//      : event.target.dataset.contact ? showNavigation(event)
+//      : event.target.dataset.projects ? showNavigation(event)
+//      : event.target.dataset.journal ? showNavigation(event)
+//      : ""
+// })
+
+// document.addEventListener('mouseout', (event)=>{
+//     event.target.dataset.dropdown ? closeNavigation(event)
+//     : ""
+
+//     console.log(event.target.dataset)
+// })
+
+
+// ███████ ██    ██ ███    ██  ██████ ████████ ██  ██████  ███    ██ ███████
+// ██      ██    ██ ████   ██ ██         ██    ██ ██    ██ ████   ██ ██
+// █████   ██    ██ ██ ██  ██ ██         ██    ██ ██    ██ ██ ██  ██ ███████
+// ██      ██    ██ ██  ██ ██ ██         ██    ██ ██    ██ ██  ██ ██      ██
+// ██       ██████  ██   ████  ██████    ██    ██  ██████  ██   ████ ███████
+
+
+// const showNavigation = (clickedItem)=> {
+
+//         if (clickedItem.target.dataset.learning) {
+//                 if (!isDisplayed.learning) {
+//                     pathListEl.style.display = "block"
+//                     isDisplayed.learning = true
+//                 }
+//         }
+
+// }
+
+// const closeNavigation = (clickedItem)=> {
+//     if (clickedItem.target.dataset.dropdown) {
+//         if (isDisplayed.learning) {
+//             pathListEl.style.display = "none"
+//             isDisplayed.learning = false
+//         }
+// }
+// }
+
+
+
+// else {
+//     pathListEl.style.display = "none"
+//     isDisplayed.learning = false
+// }
+
+
+
+
+
+
+
+
+
+
 
